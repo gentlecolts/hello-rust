b661bafca25ab03bb7e383199c2b081022a33348
Jack Phoebus <jmp1996.jp@gmail.com>
2023-09-04 14:47:10 -0400

    reformat code

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-fn foo(_x:&'static str) -> &'static str{
-    _x
+fn foo(_x: &'static str) -> &'static str {
+	_x
 }
 
 fn main() {
-    println!("Hello, {}!",foo("world"));
+	println!("Hello, {}!", foo("world"));
 }
