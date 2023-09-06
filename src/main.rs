63ea481c69443c2b6bcbeb0b83b4c992bb698b2d
Jack Phoebus <jmp1996.jp@gmail.com>
2023-09-05 23:21:16 -0400

    fibonacci

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,18 @@ fn foo(_x: &'static str) -> &'static str {
 	_x
 }
 
+fn fib(n: i32) -> i32{
+	if n<=0{
+		return fib(n+2) - fib(n+1);
+	} else 	if n<=2 {
+		return 1;
+	}
+	fib(n-1)+ fib(n-2)
+}
+
 fn main() {
 	println!("Hello, {}!", foo("world"));
+
+	let n=-7;
+	println!("{}th fibbonacci: {}",n,fib(n))
 }
