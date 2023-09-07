30ec6307e039d2afd26ce6a41b93e73092b64f5b
Jack Phoebus <jmp1996.jp@gmail.com>
2023-09-06 23:17:06 -0400

    test with closures

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,29 @@ fn fib(n: i32) -> i32{
 	fib(n-1)+ fib(n-2)
 }
 
+fn printy_thingy<T>(n: &T) where T:std::fmt::Display{
+	println!("wow, thats {}",n);
+}
+
+fn closure_test(){
+	let x=String::from("lolee");
+
+	let test1=|| printy_thingy(&x);
+	test1();
+	println!("reusing x:{}",x);
+
+	let test2=move || printy_thingy(&x);
+	test2();
+
+	//this isn't allowed
+	//println!("reusing x after move:{}",x);
+}
+
 fn main() {
 	println!("Hello, {}!", foo("world"));
 
 	let n=-7;
-	println!("{}th fibbonacci: {}",n,fib(n))
+	println!("{}th fibbonacci: {}",n,fib(n));
+
+	closure_test();
 }
